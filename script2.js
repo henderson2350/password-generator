4000b46cf1fece24715934a75b14a131586e92d6
Clare Henderson <henderson2350@gmail.com>
2021-03-19 20:57:45 -0400

    finished, pre debugging

--- script2.js
+++ script2.js
@@ -14,17 +14,14 @@ generateBtn.addEventListener("click", writePassword())
 
 // STUFF IM WRITING
 function generatePassword() {
-    var length = window.prompt("How many characters would you like?")
-    if (length < 8 || numbers > 128) {
-        numbers = window.prompt("Choose a number between 8 and 128.")
+    var numberChar = window.prompt("How many characters would you like?")
+    if (numberChar < 8 || numberChar > 128) {
+        numberChar = window.prompt("Please choose a number between 8 and 128.")
     }
     var numbers = window.confirm("Would you like to include numbers?")
     var letters = window.confirm("Would you like to include letters?")
     var special = window.confirm("Would you like to include special characters?")
-
-    if (length < 8 || numbers > 128) {
-        numbers = window.prompt("Choose a number between 8 and 128.")
-    }
+    
 
     //TODO: Use those parameters to generate a new password
     lettersArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
@@ -35,16 +32,25 @@ function generatePassword() {
     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",1,2,3,4,5,6,
     7,8,9,0]
 
+    numbersAndSpecial = [1,2,3,4,5,6,7,8,9,0, "a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    
+    lettersAndSpecial = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
     allThree = ["a", "b", "c", "d", "e", "f", "g","h", "i", 
     "j", "k", "l", "m", "n", "o", "p", "q", "r", 
-    "s", "t", "u", "v", "w", "x", "y", "z",1,2,3,4,5,6,7,8,9,0,"!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "+", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]
+    "s", "t", "u", "v", "w", "x", "y", "z",1,2,3,4,5,6,7,8,9,0,"!", "#", "$", "%", "&", "(", ")", "*",
+    "+", ",", "+", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]
 
 
     console.log(lettersAndNumbers)
 
     newPassword = ""
 
-    for (i = 0; i < length; i++) {
+    for (i = 0; i < numberChar; i++) {
 
         if (numbers && letters && special) {
             newPassword += allThree[Math.floor(Math.random() * allThree.length)]
@@ -54,8 +60,13 @@ function generatePassword() {
             newPassword += lettersAndNumbers[Math.floor(Math.random() * lettersAndNumbers.length)]
         }
 
-        // else if (numbers && special) {
-        //     newPassword += 
+        else if (numbers && special) {
+            newPassword += numbersAndSpecial[Math.floor(Math.random() * numbersAndSpecial.length)]
+        }
+
+        else if (letters && special) {
+            newPassword += lettersAndSpecial[Math.floor(Math.random() * lettersAndSpecial.length)]
+        }
     
         else if (numbers) {
                 newPassword += numbersArray[Math.floor(Math.random() * numbersArray.length)]
