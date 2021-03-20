44f25229f0f45090afdfc8d4c729eb2e07a0dedf
Clare Henderson <henderson2350@gmail.com>
2021-03-20 09:11:22 -0400

    fixed stylsheet link issue, added comments to css and js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// STUFF HE WROTE
+// Selecting the button with ID generate and linking it to the variable generateBtn
 var generateBtn = document.querySelector("#generate")
 
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-}
-
+// Adding an event listener to the generate button; when it's clicked, write the password
 generateBtn.addEventListener("click", writePassword())
 
-// STUFF IM WRITING
+// This function prompts the user to ask what sort of characters they would like to use. 
+// Then it generates a random password by choosing randomly from arrays based on their choices.
+// It then returns the newPassword.
 function generatePassword() {
+    // These prompts ask the user what type of characters they would like to use.
+    // If they choose a number that's outside of the range 8-128, they are prompted to
+    // choose another number within the range.
     var numberChar = window.prompt("How many characters would you like?")
     if (numberChar < 8 || numberChar > 128) {
         numberChar = window.prompt("Please choose a number between 8 and 128.")
@@ -20,8 +19,8 @@ function generatePassword() {
     var letters = window.confirm("Would you like to include letters?")
     var special = window.confirm("Would you like to include special characters?")
     
-
-    //TODO: Use those parameters to generate a new password
+    // These arrays list out all of the characters. I've created several arrays that
+    // cover all of the possible combinations of characters that the user can choose.
     lettersArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
     numbersArray = [0,1,2,3,4,5,6,7,8,9]
     specialArray = "!#$%&()*+,-./:;<=>?@[]^_`{|}~".split("")
@@ -43,11 +42,13 @@ function generatePassword() {
     "s", "t", "u", "v", "w", "x", "y", "z",1,2,3,4,5,6,7,8,9,0,"!", "#", "$", "%", "&", "(", ")", "*",
     "+", ",", "+", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]
 
-
-    console.log(lettersAndNumbers)
-
+    // I initialize the newPassword as a blank string, to be added to in the following for loop.
     newPassword = ""
 
+    // These if statements test for which combination of characters the user has chooses, then 
+    // randomly selects characters from the appropriate list. We iterate as many times as the 
+    // user specified they would like the length of the password to be. We concatenate each character
+    // to the end of the newPassword with each loop-through.
     for (i = 0; i < numberChar; i++) {
 
         if (numbers && letters && special) {
@@ -78,10 +79,18 @@ function generatePassword() {
             newPassword += specialArray[Math.floor(Math.random) * specialArray.length]
         }
     
-        
-
     }
-    //TODO: Return that password
+
+    // We return the new password
     return newPassword
   }
+
+// This function assigns the value returned from generatePassword to the variable password, 
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
+
 
